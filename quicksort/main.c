1595da148a29668940864460025b23569aa68d75
lim <gylim@student.42seoul.kr>
2024-08-29 20:26:02 +0900

    quicksort in c

--- quicksort/main.c
+++ quicksort/main.c
@@ -1,49 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define SWAP(ARRAY, A, B) \
-do \
-{ \
-	int tmp = ARRAY[(A)];\
-	ARRAY[(A)] = ARRAY[(B)];\
-	ARRAY[(B)] = tmp;\
-} while (0)
+#define SWAP(a, b)                                                             \
+    do {                                                                       \
+        int tmp = a;                                                           \
+        a = b;                                                                 \
+        b = tmp;                                                               \
+    } while (0)
 
-static void quicksort(int *arr, int lower, int upper)
-{
-	int left = lower;
-	int right = upper;
-	int pivot = arr[(lower + upper) / 2];
+#define SIZE (10)
 
-	do
-	{
-		while (left <= upper && arr[left] < pivot)
-			left++;
-		while (right >= lower && arr[right] > pivot)
-			right--;
-		if (left <= right)
-		{
-			SWAP(arr, left, right);
-			left++;
-			right--;
-		}
-	} while (left <= right);
-	if (lower < right)
-		quicksort(arr, lower, right);
-	if (left < upper)
-		quicksort(arr, left, upper);
-}
+void quicksort(int a[], int low, int high);
 
 int main(void)
 {
-	int N;
-	int arr[1000];
+    int a[SIZE];
+    const int size = SIZE;
 
-	scanf("%d", &N);
-	for (int i = 0; i < N; ++i)
-		scanf("%d", &arr[i]);
-	quicksort(arr, 0, N - 1);
-	for (int i = 0; i < N; ++i)
-		printf("%d\n", arr[i]);
-	return 0;
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < size; ++i) {
+        a[i] = rand() % 100;
+    }
+
+    printf("Unsorted:\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d\n", a[i]);
+    }
+
+    quicksort(a, 0, size - 1);
+
+    printf("Sorted:\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
 }
 
+/*
+ * a: a list of integers.
+ * a[low..high]
+ */
+void quicksort(int a[], int low, int high)
+{
+    int p = a[(low + high) / 2];
+    int i = low;
+    int j = high;
+
+    while (i <= j) {
+        while (a[i] < p) { // pivot의 왼쪽에서 pivot보다 크거나 같은 값을 찾는다.
+            ++i;
+        }
+        while (a[j] > p) { // pivot의 오른쪽에서 pivot보다 작거나 같은 값을 찾는다.
+            --j;
+        }
+        if (i <= j) { // i <= j 이면, 두 값을 교환해야 한다.
+            SWAP(a[i], a[j]);
+            ++i;
+            --j;
+        }
+    }
+
+    if (low < j) {
+        quicksort(a, low, j);
+    }
+    if (i < high) {
+        quicksort(a, i, high);
+    }
+}
