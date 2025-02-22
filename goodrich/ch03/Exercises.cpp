90a7fb0c1052a499030b8116ad4f201434efacd0
Gyuhyun Lim <gyuhyun.lim@outlook.com>
2025-02-22 21:56:17 +0900

    chapter 3

--- goodrich/ch03/Exercises.cpp
+++ goodrich/ch03/Exercises.cpp
@@ -1,4 +1,8 @@
 #include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include <iostream>
 
 int FindMax(int *array, int s, int n)
@@ -14,18 +18,41 @@ int FindMax(int *array, int s, int n)
   return std::max(FindMax(array, s, n / 2), FindMax(array, s + n / 2, n / 2));
 }
 
-void subset(int *set, int *subset, bool *used, int n, int cursor) {
-	
+void findSubset(int *set, int *subset, int n, int cursor, int index)
+{
+  if (index == n) {
+    for (int i = 0; i < cursor; ++i) {
+      std::cout << subset[i] << ' ';
+    }
+    std::cout << '\n';
+    return;
+  }
+
+  findSubset(set, subset, n, cursor, index + 1);
+  subset[cursor] = set[index];
+  findSubset(set, subset, n, cursor + 1, index + 1);
 }
 
-void PrintAllSubsets(int *set, int n) {}
+void printAllSubsets(int *set, int n)
+{
+  int  *subset = new int[n];
+  std::memset(subset, 0, n * sizeof(int));
+
+  findSubset(set, subset, n, 0, 0);
+}
 
 int main(void)
 {
-  int array[11];
-  for (int i = 0; i < 11; ++i) {
-    array[i] = i;
+  std::srand(std::time(NULL));
+
+  int array[3];
+  for (int i = 0; i < 3; ++i) {
+    array[i] = std::rand() % 19837;
   }
   std::cout << FindMax(array, 0, 11);
+  std::cout << "\n\n";
+
+  printAllSubsets(array, sizeof(array) / sizeof(int));
+
   return 0;
 }
